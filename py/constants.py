1ae69d4a978359af17bc3c1cd8d13716472b5efa
Chris Goad <cagoad@gmail.com>
2013-08-21 13:14:27 -0600

    python removed

--- py/constants.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-maintainenceMode = False
-#storageDir = "/mnt/ebs1/termite/storage"
-
-password = "vMfm7i1r" # primitive protection for now
-maxHourlySaves = 10000
-sessionTimeout = 6 * 3600
--- py/dispatch.wsgi
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-
-import sys
-pydir = "/mnt/ebs0/prototypejungle/py";
-sys.path.append(pydir)
-execfile(pydir + "/dispatch.py")
-
