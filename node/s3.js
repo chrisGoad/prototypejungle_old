aa5905da1bed0e93dd91ff9418e5ef358d994959
Chris Goad <cagoad@gmail.com>
2013-08-22 09:35:57 -0600

    throttle saves to 1000 per hour

--- node/s3.js
+++ node/s3.js
@@ -9,6 +9,7 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./keys/aws.json');
 
 var util = require('./util.js');
+var pjdb = require('./db.js').pjdb;
 
 var fs = require('fs');
 var buffer = require('buffer');
@@ -17,27 +18,64 @@ var page = require('./page.js');
 var user = require('./user.js');
 var session = require('./session.js');
 
+var maxSavesPerHour = 1000;
+
+var countSaves = function (cb) {
+  var hrs = Math.floor(Date.now()/(3600*1000))-382548; // since a particular hour on 8/22/2013
+  var ky = "s3count_"+hrs;
+  pjdb.get(ky,function (e,d) {
+    if (e) {
+      var cnt = 1;
+    } else {
+      var cnt = parseInt(d)+1;
+    }
+    if (cnt > maxSavesPerHour) {
+      cb("saveCountExceeded");
+      return;
+    }
+    util.log("s3","Save count for hrs="+hrs+" bumped to ",cnt);
+    pjdb.put(ky,cnt,function (e,d) {
+      if (cb) {
+        cb(cnt);
+      }
+    });
+  });
+}
+
+// call back returns "s3error","countExceeded", or 1 for success
 exports.save = function (path,value,contentType,encoding,cb) {
-  var S3 = new AWS.S3(); // if s3 is not rebuilt, it seems to lose credentials, somehow
-  util.log("s3","save to s3 at ",path," with contentType",contentType,"encoding",encoding);
-  var bf = new buffer.Buffer(value,encoding);
-  if (path[0]=="/") {
-    path = path.substr(1);
-  }
-  var p = {
-    Bucket:pj_bucket,
-    Body:bf,
-    ContentType:contentType,
-    ACL:'public-read',
-    Key:path
-  }
-  S3.putObject(p,cb);
+  countSaves(function (cnt) {
+    var S3 = new AWS.S3(); // if s3 is not rebuilt, it seems to lose credentials, somehow
+    util.log("s3","save to s3 at ",path," with contentType",contentType,"encoding",encoding);
+    var bf = new buffer.Buffer(value,encoding);
+    if (path[0]=="/") {
+      path = path.substr(1);
+    }
+    var p = {
+      Bucket:pj_bucket,
+      Body:bf,
+      ContentType:contentType,
+      ACL:'public-read',
+      Key:path
+    }
+    S3.putObject(p,function (e,d) {
+      if (e) {
+        util.log("error",e);
+        cb("s3error");
+      } else if (cnt == "saveCountExceeded") {
+        cb(cnt);
+      } else {
+        cb(1);
+      }
+    });
+  });
 }
 
 var beginsWith = function (s,p) {
   var ln = p.length;
   return s.substr(0,ln)==p;
 }
+
 
 
 var viewToS3 = function(pth,cb) {
@@ -89,16 +127,25 @@ exports.saveHandler = function (request,response,cob) {
       }
       util.log("s3"," s3 save",path,ctp,encoding);
 
-      exports.save(path,vl,ctp, encoding,function (e,d) {
-        util.log("s3","FROM s3 save",e,d);
+      exports.save(path,vl,ctp, encoding,function (x) {
+        util.log("s3","FROM s3 save",x);
+        if ((typeof x!="number")) {
+          page.failResponse(response,x);
+          return;
+        }
         vwf = cob.viewFile;
         if (vwf) {
-          viewToS3(vwf,function (e,d) {
-            util.log("s3","FROM viewTOS3",e,d);
-            page.okResponse(response);
+          viewToS3(vwf,function (x) {
+            util.log("s3","FROM viewTOS3",x);
+            if ((typeof x=="number")) {
+              page.okResponse(response);
+            } else {
+              page.failResponse(response,x);
+            }
           });
+        } else {
+          page.okResponse(response);
         }
-        page.okResponse(response);
       });
     });
   });
