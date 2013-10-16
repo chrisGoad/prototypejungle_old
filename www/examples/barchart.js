57d32d01d8e1158df2e71228263769b2da78b93e
Chris Goad <cagoad@gmail.com>
2013-10-16 11:58:36 -0600

    minor

--- www/examples/barchart.js
+++ www/examples/barchart.js
@@ -8,7 +8,7 @@
     cnv.fitFactor = 0.7;
     draw.installAsRoot(itemUrl,cnv,function (rs) {
       // modify the height of the all of the labels
-      // by changing the relevant
+      // by changing the relevant prototype
       rs.LabelP.style.height= 20;
       // adjust offsets
       rs.xAxis.textOffset=30;
