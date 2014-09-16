f5edb1984c6785ea9b23465483badc12e8c083a8
Chris Goad <cagoad@gmail.com>
2014-09-16 07:27:35 -0600

    minor fixes in index.html tech.html

--- node/admin/updateS3.js
+++ node/admin/updateS3.js
@@ -188,8 +188,10 @@ if (a0 === "p") {
   //           ["choosedoc.html",htt],["tech.html",htt],["userguide.html",htt],["about.html",htt]];
   
   var fts = [{source:"style.css",ctype:"text/css"}];
-  add1Html(fts,"index.html","tindex.html");
-  add1Html(fts,"notyet.html","index.html");
+  add1Html(fts,"index.html","index.html");
+
+  //add1Html(fts,"index.html","tindex.html");
+  //add1Html(fts,"notyet.html","index.html");
   addHtml(fts,["inspect","newuser","view","chooser.html","unsupportedbrowser","missing.html","limit.html","denied.html"]);
   //  addJsFiles(fts,["min","draw","core","common1","common2","inspect","view","chooser2"]);
   //var fts = [];
