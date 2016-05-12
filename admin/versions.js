e9564bfa835117a968b1e698b204fde88d5a92b3
Chris Goad <cagoad@gmail.com>
2016-05-12 10:43:47 -0600

    firebase transition

---
+++ admin/versions.js
@@ -0,0 +1,21 @@
+
+
+exports.pjcore = "0.9.3";
+exports.pjdom = "0.9.3";
+exports.pjui = "0.9.3";
+exports.pjworker = "0.9.3";
+exports.topbar = "0.9.3";
+exports.pjpage = "0.9.3";
+
+//exports.pjinsert = "0.8.0";
+//exports.pjinspect = "0.8.0";
+exports.pjdraw = "0.8.2";
+exports.pjdev = "0.8.2";
+exports.pjchooser = "0.9.3";
+exports.pjview = "0.9.2";
+exports.pjloginout = "0.8.2";
+
+exports.chart = "0.9.3";
+exports.editor = "0.9.3";
+
+
